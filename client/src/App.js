107754ab40b99e3c56aa97e94539fca0f1f3bb26
Steve Lin <steve.lin@fullerton.com.tw>
2023-07-12 21:40:37 +0800

    test

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Test
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
